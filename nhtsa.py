6845ab85730535550bbd55f30d1ee790b59c1b64
Josh Todd <josht@tune.com>
2017-08-29 14:52:05 -0700

    Create nhtsa.py

---
+++ nhtsa.py
@@ -0,0 +1,17 @@
+from urllib2 import urlopen
+from json import load
+
+apiUrl = "http://www.nhtsa.gov/webapi/api/SafetyRatings"
+apiParam = "/vehicleid/5782"
+outputFormat = "?format=json"
+
+response = urlopen(apiUrl + apiParam + outputFormat)
+
+json_obj = load(response)
+
+print '------------------------------'
+print '           Result             '
+print '------------------------------'
+for objectCollection in json_obj['Results']:
+	for safetyRatingAttribute, safetyRatingValue in objectCollection.iteritems():
+		print safetyRatingAttribute, ": ", safetyRatingValue
